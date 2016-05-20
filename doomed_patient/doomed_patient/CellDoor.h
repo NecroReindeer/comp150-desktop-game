5d9014cb288418c255addb9e301615b36295c771
NecroReindeer <necroreindeer@gmail.com>
2016-05-20 14:27:39 +0100

    Added doxygen comment to CellDoor.h

--- doomed_patient/doomed_patient/CellDoor.h
+++ doomed_patient/doomed_patient/CellDoor.h
@@ -1,3 +1,9 @@
+//! Class for doors.
+/*!
+  This class represents the door of the cells.
+  It inherits from CellEdge and implements the method
+  for setting the sprite.
+*/
 #pragma once
 #include "CellEdge.h"
 
@@ -5,8 +11,13 @@ class CellDoor :
 	public CellEdge
 {
 public:
+	//! Constructor for the door.
+	/*!
+	  This is the constructor for the door.
+	  It assigns the door a direction and a cell, as
+	  well as a pointer to the game.
+	*/
 	CellDoor(Directions::Direction direction, std::shared_ptr<LevelCell> cell, PatientGame* game);
-	~CellDoor();
 
 	//! This method returns whether the edge is a wall.
 	/*!
